f81322bf152b26be61cd3bb5e0aaa83c562070d4
Michał Kozłowski <micho.kozlowski@gmail.com>
2025-06-07 21:47:17 +0200

    teraz git

---
+++ src/test/java/SomeClass.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class SomeClass {
+    @Test
+    public void test1(){
+        int x=1;
+        int y=2;
+        assert x+1==y;
+    }
+
+    @Test
+    public void test2(){
+        int x=2;
+        int y=3;
+        assert x*y==6;
+    }
+}
